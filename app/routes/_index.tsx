159514666267333376ac237c4d531e501a7e7af3
KerryBoyko <kerry.ann.boyko@gmail.com>
2023-07-22 16:45:13 -0400

    Fix markdown

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,24 +1,19 @@
-import type { LinksFunction, LoaderFunction } from "@remix-run/node";
+import { type LinksFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
-import MarkdownDisplay from "~/components/Markdown/MarkdownDisplay";
-import { readFromMDFile } from "~/logic/readFromMDFile.server";
 import aboutStyles from "~/styles/about.css";
+import AboutMarkdown from "~/static/markdown/about.md";
 
 export const links: LinksFunction = () =>
   [aboutStyles].map((href) => ({ rel: "stylesheet", href }));
 
-export const loader: LoaderFunction = async ({ request }) => {
-  const markdown = await readFromMDFile("about.md");
-  return { markdown };
-};
-
 export default function AboutRoute() {
-  const { markdown } = useLoaderData();
   return (
     <>
       <main>
         <div className="about">
-          <MarkdownDisplay className="about__text" markdown={markdown} />
+          <div className="markdown-display content-box">
+            <AboutMarkdown className="markdown-display" />
+          </div>
         </div>
       </main>
     </>
