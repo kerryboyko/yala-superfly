8aed97ab8823451a311a6e13f3234fd310a723c9
KerryBoyko <kerry.ann.boyko@gmail.com>
2023-08-07 08:59:01 -0400

    Hotfix prettier

--- app/routes/_layout._index.tsx
+++ app/routes/_layout._index.tsx
@@ -2,6 +2,7 @@ import type { LinksFunction } from "@remix-run/node";
 import { NavLink } from "@remix-run/react";
 
 import FormatMarkdownFile from "~/components/Markdown/FormatMarkdownFile";
+import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 import AboutPage from "~/static/markdown/about.md";
 import IntroPage from "~/static/markdown/intro.md";
 import RoadmapPage from "~/static/markdown/roadmap.md";
@@ -19,20 +20,43 @@ export default function IndexRoute() {
             <NavLink to="./#intro">Intro</NavLink>
           </li>
           <li>
-            <NavLink to="./#roadmap">Roadmap</NavLink>
+            <NavLink to="./#about">About</NavLink>
           </li>
           <li>
-            <NavLink to="./#about">About</NavLink>
+            <NavLink to="./#roadmap">Roadmap</NavLink>
           </li>
         </ul>
         <section id="intro">
-          <IntroPage />
+          <Card>
+            <CardHeader>
+              <CardTitle>Intro:</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <IntroPage />
+            </CardContent>
+          </Card>
         </section>
-        <section id="roadmap">
-          <RoadmapPage />
-        </section>
+        <hr />
         <section id="about">
-          <AboutPage />
+          <Card>
+            <CardHeader>
+              <CardTitle>About:</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <AboutPage />
+            </CardContent>
+          </Card>
+        </section>
+        <hr />
+        <section id="roadmap">
+          <Card>
+            <CardHeader>
+              <CardTitle>Roadmap:</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <RoadmapPage />
+            </CardContent>
+          </Card>
         </section>
       </FormatMarkdownFile>
     </>
